88b7adb985d8cfb1ef70bb492732ff601b021941
Jhon Arlem Andrade <jhondoncan@outlook.com>
2022-11-23 11:39:37 -0500

    [FEAT] Connects frontend to backend, now allows login and save data in localstorage

--- FrontendMascotas/src/app/modulos/seguridad/login/login.component.ts
+++ FrontendMascotas/src/app/modulos/seguridad/login/login.component.ts
@@ -1,16 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { Router } from "@angular/router";
+import { SeguridadService } from "src/app/servicios/seguridad.service";
+import Swal from "sweetalert2";
+const cryptoJS = require("crypto-js");
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
- focus;
+  focus;
   focus1;
-  constructor() { }
+
+  formularioLogin = new FormGroup({
+    correo: new FormControl(""),
+    password: new FormControl(""),
+  });
+
+  constructor(
+    private servicioSeguridad: SeguridadService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    if (this.servicioSeguridad.obtenerSession()) {
+      this.router.navigate(["/inicio"]);
+    }
   }
 
+  identificarUsuario() {
+    let correo = this.formularioLogin.value.correo;
+    let password = this.formularioLogin.value.password;
+    /* let passwordCifrada = cryptoJS.MD5(password).toString(); */
+    this.servicioSeguridad.iniciarSesion(correo, password).subscribe(
+      (datos: any) => {
+        // ok
+        this.servicioSeguridad.almacenarSession(datos);
+        this.router.navigate(["/inicio"]);
+      },
+      (error: any) => {
+        // error
+        this.credencialesIncorrectas();
+      }
+    );
+  }
+
+  /* MENSAJES SWEETALERT */
+  credencialesIncorrectas() {
+    Swal.fire({
+      title: "",
+      text: "Correo o contraseña incorrectos.",
+      icon: "error",
+      confirmButtonColor: "#5E72E4",
+      confirmButtonText: "Aceptar",
+    });
+  }
+
+  credencialesCorrectas() {
+    Swal.fire({
+      title: "",
+      text: "Bienvenido al sistema.",
+      icon: "success",
+      confirmButtonColor: "#5E72E4",
+      confirmButtonText: "Aceptar",
+    });
+  }
 }
--- FrontendMascotas/src/app/modulos/seguridad/seguridad.module.ts
+++ FrontendMascotas/src/app/modulos/seguridad/seguridad.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { SeguridadRoutingModule } from './seguridad-routing.module';
-import { LoginComponent } from './login/login.component';
-import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
-
+import { SeguridadRoutingModule } from "./seguridad-routing.module";
+import { LoginComponent } from "./login/login.component";
+import { RecuperarClaveComponent } from "./recuperar-clave/recuperar-clave.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RecuperarClaveComponent
-  ],
+  declarations: [LoginComponent, RecuperarClaveComponent, LogoutComponent],
   imports: [
     CommonModule,
-    SeguridadRoutingModule
-  ]
+    SeguridadRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class SeguridadModule { }
+export class SeguridadModule {}
