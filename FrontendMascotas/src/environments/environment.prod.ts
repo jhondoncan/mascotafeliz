ab5a232839c9887210070ab9252678294dbcde3a
Jhon Arlem Andrade <jhondoncan@outlook.com>
2022-11-24 19:03:18 -0500

    [FIX] Environment variable updated to production

--- FrontendMascotas/src/environments/environment.prod.ts
+++ FrontendMascotas/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: "http://localhost:3000",
+  /* baseUrl: "http://localhost:3000", */
+  baseUrl: "https://mascotafeliz-back.onrender.com",
 };
